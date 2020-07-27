db78dc0504f9de404dde4a6e1c04fe83d4e75574
Anurag Reddy <anuragreddy@Anurags-MacBook-Pro.local>
2020-07-27 14:14:13 +0530

    initial

---
+++ naruto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  naruto
+//
+//  Created by Anurag Reddy on 04/07/20.
+//  Copyright © 2020 Anurag Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
